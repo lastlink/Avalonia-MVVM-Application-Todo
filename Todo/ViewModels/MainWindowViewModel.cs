44d9a45fa31ef6a4b838be4ad50f96ccf26d1a89
lastlink <theefunk@gmail.com>
2020-02-09 18:43:28 -0500

    working todo app

--- Todo/ViewModels/MainWindowViewModel.cs
+++ Todo/ViewModels/MainWindowViewModel.cs
@@ -1,18 +1,49 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Text;
+using Todo.Models;
 using Todo.Services;
 
 namespace Todo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        ViewModelBase content;
+
         public MainWindowViewModel(Database db)
         {
-            List = new TodoListViewModel(db.GetItems());
+            Content = List = new TodoListViewModel(db.GetItems());
+        }
+
+        public ViewModelBase Content
+        {
+            get => content;
+            private set => this.RaiseAndSetIfChanged(ref content, value);
         }
 
         public TodoListViewModel List { get; }
-        //public string Greeting => "Welcome to Avalonia!";
+
+        public void AddItem()
+        {
+            var vm = new AddItemViewModel();
+
+            Observable.Merge(
+                vm.Ok,
+                vm.Cancel.Select(_ => (TodoItem)null))
+                .Take(1)
+                .Subscribe(model =>
+                {
+                    if (model != null)
+                    {
+                        List.Items.Add(model);
+                    }
+
+                    Content = List;
+                });
+
+            Content = vm;
+        }
     }
 }
